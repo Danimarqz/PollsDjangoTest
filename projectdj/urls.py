c64b713ae020da4085250780460cf6d6f1ef9f9d
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-06-25 23:36:09 +0200

    views / urls

--- projectdj/urls.py
+++ projectdj/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
