90b3b46b35ff6b3324f8745ccd5ce2802a534273
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-06-25 20:46:21 +0200

    admin page added

--- polls/models.py
+++ polls/models.py
@@ -1,14 +1,22 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
 
 class User(models.Model):
     name = models.CharField(max_length=200)
@@ -18,3 +26,5 @@ class User(models.Model):
     address = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
