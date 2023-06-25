6da0eeb9dac4662f5357dc7f636a4a443c2ff92b
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-06-25 19:45:00 +0200

    Connected with DB, polls created

---
+++ polls/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
