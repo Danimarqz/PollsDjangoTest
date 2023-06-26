8def816c05907e59052a2f91874faafe26660149
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-06-26 23:59:48 +0200

    views changed to generic, urls and ultra basic html added

--- projectdj/urls.py
+++ projectdj/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('', include('polls.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
